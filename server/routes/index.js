406afb1646cfaebebf0ced973d5cade544acb2b6
MichaelShawUK <MichaelShawUK@gmail.com>
2023-04-15 22:18:21 +0100

    Create JWT for authenticated users and verify JWT on protected route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const user = require("./user");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 router.get("/", function (req, res, next) {
   res.json({ message: "Welcome to Blog API" });
@@ -47,10 +49,45 @@ router.post("/login", async (req, res, next) => {
       return res.send("Incorrect password");
     }
 
+    const token = createToken({
+      sub: user.id,
+      user: {
+        name: user.name,
+      },
+    });
+    console.log(token);
+
     res.send("Successful login");
   } catch (err) {
     return next(err);
   }
+});
+
+const createToken = (payload) => {
+  const { PRIVATE_KEY } = process.env;
+  return jwt.sign(payload, PRIVATE_KEY, { algorithm: "RS256" });
+};
+
+router.get("/protected", (req, res, next) => {
+  const { PUBLIC_KEY } = process.env;
+  const authHeader = req.get("Authorization");
+
+  if (!authHeader) {
+    return res.json({
+      success: false,
+      message: "Token not supplied",
+    });
+  }
+
+  const token = authHeader.slice(7);
+
+  jwt.verify(token, PUBLIC_KEY, { algorithm: "RS256" }, (err, decoded) => {
+    if (err) {
+      return res.json(err);
+    }
+    req.decoded = decoded;
+    res.json(decoded);
+  });
 });
 
 const index = router;
