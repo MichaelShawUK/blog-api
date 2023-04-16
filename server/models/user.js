1edee438a1a2800af3142217757a34bf18aad8fd
MichaelShawUK <MichaelShawUK@gmail.com>
2023-04-16 17:37:27 +0100

    Refactor endpoints to return JSON object with minimum success and message field

--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String },
-  password: { type: String },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
   name: { type: String },
 });
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,16 +6,15 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-router.get("/", function (req, res, next) {
-  res.json({ message: "Welcome to Blog API" });
-});
-
 router.post("/register", async (req, res, next) => {
   try {
     const { username, password, name } = req.body;
 
     if (await User.findOne({ username })) {
-      return res.send("User already exists");
+      return res.json({
+        success: false,
+        message: "User already exists",
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -27,9 +26,15 @@ router.post("/register", async (req, res, next) => {
     });
 
     await user.save();
-    res.send(`${username} has been registered...`);
+    res.json({
+      success: true,
+      message: `${username} has been registered`,
+    });
   } catch (err) {
-    return next(err);
+    return res.json({
+      success: false,
+      message: "Unable to register user",
+    });
   }
 });
 
@@ -40,35 +45,51 @@ router.post("/login", async (req, res, next) => {
     const user = await User.findOne({ username });
 
     if (!user) {
-      return res.send("User does not exist");
+      return res.json({
+        success: false,
+        message: "User does not exist",
+      });
     }
 
     const match = await bcrypt.compare(password, user.password);
 
     if (!match) {
-      return res.send("Incorrect password");
+      return res.json({
+        success: false,
+        message: "Incorrect password",
+      });
     }
 
-    const token = createToken({
+    const { PRIVATE_KEY } = process.env;
+    const payload = {
       sub: user.id,
       user: {
         name: user.name,
       },
-    });
-    console.log(token);
+    };
 
-    res.send("Successful login");
+    jwt.sign(payload, PRIVATE_KEY, { algorithm: "RS256" }, (err, token) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: "Error creating token",
+        });
+      }
+      return res.json({
+        success: true,
+        message: "Successful login,",
+        token,
+      });
+    });
   } catch (err) {
-    return next(err);
+    return res.json({
+      success: false,
+      message: "Unable to log user in",
+    });
   }
 });
 
-const createToken = (payload) => {
-  const { PRIVATE_KEY } = process.env;
-  return jwt.sign(payload, PRIVATE_KEY, { algorithm: "RS256" });
-};
-
-router.get("/protected", (req, res, next) => {
+const isTokenValid = (req, res, next) => {
   const { PUBLIC_KEY } = process.env;
   const authHeader = req.get("Authorization");
 
@@ -83,10 +104,23 @@ router.get("/protected", (req, res, next) => {
 
   jwt.verify(token, PUBLIC_KEY, { algorithm: "RS256" }, (err, decoded) => {
     if (err) {
-      return res.json(err);
+      return res.json({
+        success: false,
+        message: "Could not verify token",
+        error: err,
+      });
     }
-    req.decoded = decoded;
-    res.json(decoded);
+    req.payload = decoded;
+    next();
+  });
+};
+
+router.get("/protected", isTokenValid, (req, res, next) => {
+  res.json({
+    success: true,
+    message: "Accessing protected route",
+    id: req.payload.sub,
+    name: req.payload.user.name,
   });
 });
 
