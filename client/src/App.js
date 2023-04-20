0e50f738a95bac2827072fe593fa11c0dd27e4b8
MichaelShawUK <MichaelShawUK@gmail.com>
2023-04-20 18:13:36 +0100

    BlogPreview components uses data from API call

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,15 @@ function App() {
   useEffect(() => {
     console.log("RUNS ONCE");
     axios.get("http://localhost:3000/posts").then((res) => {
-      console.log(res.data);
       setPosts(res.data.posts);
     });
   }, []);
 
   return (
     <div className="App">
-      <BlogPreview />
-      {posts.map((post) => {
-        return (
-          <div key={post._id}>
-            <h1>{post.title}</h1>
-            <p>
-              Posted by <strong>{post.author.name}</strong>
-            </p>
-            <p>{post.body}</p>
-          </div>
-        );
-      })}
+      {posts.map((post) => (
+        <BlogPreview key={post._id} post={post} />
+      ))}
     </div>
   );
 }
--- client/src/components/BlogPreview.js
+++ client/src/components/BlogPreview.js
@@ -1,22 +1,24 @@
 import "../index.css";
 
-const BlogPreview = () => {
+const BlogPreview = (post) => {
+  const { title, body, updatedAt, imageUrl } = post.post;
+
+  const date = new Date(updatedAt);
+
+  const dateFormatted = date.toLocaleDateString("en-us", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
   return (
     <div className="blog-preview">
-      <img
-        src="https://res.cloudinary.com/dzpobfxwj/image/upload/v1682002860/blog/abstract3_hoyu66.jpg"
-        alt=""
-        className="blog-img"
-      />
-      <div className="date-posted">April 19, 2023</div>
-      <div className="blog-title">
-        Axios vs. fetch: Performing HTTP requests
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at turpis
-        tincidunt, hendrerit velit at, luctus nulla. Cras accumsan augue eu odio
-        tempor, et maximus.
-      </div>
+      <a href={`/${post.post._id}`}>
+        <img src={imageUrl} alt="" className="blog-img" />
+        <div className="date-posted">{dateFormatted}</div>
+        <div className="blog-title">{title}</div>
+        <div>{body}</div>
+      </a>
     </div>
   );
 };
