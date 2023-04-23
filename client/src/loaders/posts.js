667e00d434a9a620c91113c9e02ea50fdef73479
MichaelShawUK <MichaelShawUK@gmail.com>
2023-04-23 13:06:11 +0100

    Change textarea font-family to match app font

--- client/src/loaders/posts.js
+++ client/src/loaders/posts.js
@@ -2,11 +2,9 @@ import axios from "axios";
 
 const loader = async () => {
   try {
-    const res = await axios.get("http://localhost:3000/postss");
-    // console.log(res);
+    const res = await axios.get("http://localhost:3000/posts");
     return [null, res];
   } catch (err) {
-    console.log(err);
     return [err, null];
   }
 };
