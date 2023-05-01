67ac97b126537b039ad8dfb27f4aa6a99c21ecd9
MichaelShawUK <MichaelShawUK@gmail.com>
2023-05-01 10:59:56 +0100

    Prevent user from submitting empty comment

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -1,39 +1,34 @@
 import { Form, useParams } from "react-router-dom";
-
-const showForm = (e) => {
-  const commentForm = document.getElementById("comment-form-container");
-  commentForm.classList.remove("displayNone");
-  e.target.style = "display: none";
-};
-
-const hideForm = (e) => {
-  const commentForm = document.getElementById("comment-form-container");
-  commentForm.classList.add("displayNone");
-};
+import { useState } from "react";
 
 const CommentForm = () => {
+  const [showCommentForm, setShowCommentForm] = useState(false);
   const { postId } = useParams();
   return (
     <>
-      <button onClick={showForm} className="add-comment-btn">
-        Add Comment
-      </button>
-      <div id="comment-form-container" className="displayNone">
+      {!showCommentForm && (
+        <button
+          onClick={() => setShowCommentForm(true)}
+          className="add-comment-btn"
+        >
+          Add Comment
+        </button>
+      )}
+      {showCommentForm && (
         <Form
           action={`/posts/${postId}/comments`}
           method="post"
           id="comment-form"
+          onSubmit={() => setShowCommentForm(false)}
         >
           <input name="post" type="text" value={postId} hidden readOnly></input>
           <label htmlFor="author">Name</label>
-          <input id="author" name="author" type="text"></input>
+          <input id="author" name="author" type="text" required></input>
           <label htmlFor="body">Comment</label>
-          <textarea id="body" name="body" rows={10}></textarea>
-          <button type="submit" onClick={hideForm}>
-            Submit Comment
-          </button>
+          <textarea id="body" name="body" rows={10} required></textarea>
+          <button type="submit">Submit Comment</button>
         </Form>
-      </div>
+      )}
     </>
   );
 };
